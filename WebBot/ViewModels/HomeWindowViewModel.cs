c7222658bb0766e7ad54f3abc388a71d5b4201c3
Murad <murad.yunus.2017@gmail.com>
2023-06-17 22:20:13 +0400

    SimpleChanges

--- WebBot/ViewModels/HomeWindowViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebBot.Commands;
-
-namespace WebBot.ViewModels
-{
-    public class HomeWindowViewModel
-    {
-        public GetDataCommand GetDataCommand => new GetDataCommand(this);
-    }
-}
